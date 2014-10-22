330dc4575f04868022ac5996997110b3d5260ef9
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-10-22 16:30:36 -0700

    Small changes, need to cleanup and rebase later

--- src/main/java/com/github/dockerjava/core/command/AbstrDockerCmd.java
+++ src/main/java/com/github/dockerjava/core/command/AbstrDockerCmd.java
@@ -9,20 +9,23 @@ import com.github.dockerjava.api.command.DockerCmdExec;
 import com.google.common.base.Preconditions;
 
 public abstract class AbstrDockerCmd<CMD_T extends DockerCmd<RES_T>, RES_T> implements DockerCmd<RES_T> {
-    
+
     private final static Logger LOGGER = LoggerFactory.getLogger(AbstrDockerCmd.class);
 
-	protected DockerCmdExec<CMD_T, RES_T> execution;
-	
-	public AbstrDockerCmd(DockerCmdExec<CMD_T, RES_T> execution) {
-		Preconditions.checkNotNull(execution, "execution was not specified");
-		this.execution = execution;
-	}
+    protected DockerCmdExec<CMD_T, RES_T> execution;
+
+    protected AbstrDockerCmd() {
+    }
+
+    public AbstrDockerCmd(DockerCmdExec<CMD_T, RES_T> execution) {
+        Preconditions.checkNotNull(execution, "execution was not specified");
+        this.execution = execution;
+    }
 
     @Override
     @SuppressWarnings("unchecked")
-	public RES_T exec() throws DockerException {
-		LOGGER.debug("Cmd: {}", this);
-		return execution.exec((CMD_T)this);
-	}
+    public RES_T exec() throws DockerException {
+        LOGGER.debug("Cmd: {}", this);
+        return execution.exec((CMD_T)this);
+    }
 }
--- src/main/java/com/github/dockerjava/core/command/CreateContainerCmdImpl.java
+++ src/main/java/com/github/dockerjava/core/command/CreateContainerCmdImpl.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import com.github.dockerjava.api.ConflictException;
 import com.github.dockerjava.api.NotFoundException;
 import com.github.dockerjava.api.command.CreateContainerCmd;
@@ -13,7 +12,6 @@ import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.ExposedPorts;
 import com.github.dockerjava.api.model.Volume;
 import com.github.dockerjava.api.model.Volumes;
-
 import com.google.common.base.Preconditions;
 
 /**
@@ -23,9 +21,9 @@ import com.google.common.base.Preconditions;
  */
 public class CreateContainerCmdImpl extends AbstrDockerCmd<CreateContainerCmd, CreateContainerResponse> implements CreateContainerCmd  {
 
-	private String name;
-	
-	@JsonProperty("Hostname")     private String    hostName = "";
+    private String name;
+
+    @JsonProperty("Hostname")     private String    hostName = "";
     @JsonProperty("User")         private String    user = "";
     @JsonProperty("Memory")       private long      memoryLimit = 0;
     @JsonProperty("MemorySwap")   private long      memorySwap = 0;
@@ -46,274 +44,277 @@ public class CreateContainerCmdImpl extends AbstrDockerCmd<CreateContainerCmd, C
     @JsonProperty("WorkingDir")   private String workingDir = "";
     @JsonProperty("DisableNetwork") private boolean disableNetwork = false;
     @JsonProperty("ExposedPorts")   private ExposedPorts exposedPorts = new ExposedPorts();
-	
-	public CreateContainerCmdImpl(CreateContainerCmd.Exec exec, String image) {
-		super(exec);
-		Preconditions.checkNotNull(image, "image was not specified");
-		withImage(image);
-	}
 
-	@Override
-	public CreateContainerCmdImpl withName(String name) {
-		Preconditions.checkNotNull(name, "name was not specified");
+    protected CreateContainerCmdImpl() {
+    }
+
+    public CreateContainerCmdImpl(CreateContainerCmd.Exec exec, String image) {
+        super(exec);
+        Preconditions.checkNotNull(image, "image was not specified");
+        withImage(image);
+    }
+
+    @Override
+    public CreateContainerCmdImpl withName(String name) {
+        Preconditions.checkNotNull(name, "name was not specified");
         this.name = name;
         return this;
     }
 
     @Override
-	public String getName() {
+    public String getName() {
         return name;
     }
 
     @Override
-	public CreateContainerCmdImpl withExposedPorts(ExposedPort... exposedPorts) {
+    public CreateContainerCmdImpl withExposedPorts(ExposedPort... exposedPorts) {
         this.exposedPorts = new ExposedPorts(exposedPorts);
         return this;
     }
 
     @Override
-	@JsonIgnore
+    @JsonIgnore
     public ExposedPort[] getExposedPorts() {
         return exposedPorts.getExposedPorts();
     }
 
 
     @Override
-	public boolean isDisableNetwork() {
+    public boolean isDisableNetwork() {
         return disableNetwork;
     }
 
     @Override
-	public String getWorkingDir() { 
-    	return workingDir;
+    public String getWorkingDir() {
+        return workingDir;
     }
 
     @Override
-	public CreateContainerCmdImpl withWorkingDir(String workingDir) {
+    public CreateContainerCmdImpl withWorkingDir(String workingDir) {
         this.workingDir = workingDir;
         return this;
     }
 
 
     @Override
-	public String getHostName() {
+    public String getHostName() {
         return hostName;
     }
 
     @Override
-	public CreateContainerCmdImpl withDisableNetwork(boolean disableNetwork) {
+    public CreateContainerCmdImpl withDisableNetwork(boolean disableNetwork) {
         this.disableNetwork = disableNetwork;
         return this;
     }
 
     @Override
-	public CreateContainerCmdImpl withHostName(String hostName) {
+    public CreateContainerCmdImpl withHostName(String hostName) {
         this.hostName = hostName;
         return this;
     }
 
     @Override
-	public String[] getPortSpecs() {
+    public String[] getPortSpecs() {
         return portSpecs;
     }
 
     @Override
-	public CreateContainerCmdImpl withPortSpecs(String... portSpecs) {
+    public CreateContainerCmdImpl withPortSpecs(String... portSpecs) {
         this.portSpecs = portSpecs;
         return this;
     }
 
     @Override
-	public String getUser() {
+    public String getUser() {
         return user;
     }
 
     @Override
-	public CreateContainerCmdImpl withUser(String user) {
+    public CreateContainerCmdImpl withUser(String user) {
         this.user = user;
         return this;
     }
 
     @Override
-	public boolean isTty() {
+    public boolean isTty() {
         return tty;
     }
 
     @Override
-	public CreateContainerCmdImpl withTty(boolean tty) {
+    public CreateContainerCmdImpl withTty(boolean tty) {
         this.tty = tty;
         return this;
     }
 
     @Override
-	public boolean isStdinOpen() {
+    public boolean isStdinOpen() {
         return stdinOpen;
     }
 
     @Override
-	public CreateContainerCmdImpl withStdinOpen(boolean stdinOpen) {
+    public CreateContainerCmdImpl withStdinOpen(boolean stdinOpen) {
         this.stdinOpen = stdinOpen;
         return this;
     }
 
     @Override
-	public boolean isStdInOnce() {
+    public boolean isStdInOnce() {
         return stdInOnce;
     }
 
     @Override
-	public CreateContainerCmdImpl withStdInOnce(boolean stdInOnce) {
+    public CreateContainerCmdImpl withStdInOnce(boolean stdInOnce) {
         this.stdInOnce = stdInOnce;
         return this;
     }
 
     @Override
-	public long getMemoryLimit() {
+    public long getMemoryLimit() {
         return memoryLimit;
     }
 
     @Override
-	public CreateContainerCmdImpl withMemoryLimit(long memoryLimit) {
+    public CreateContainerCmdImpl withMemoryLimit(long memoryLimit) {
         this.memoryLimit = memoryLimit;
         return this;
     }
 
     @Override
-	public long getMemorySwap() {
+    public long getMemorySwap() {
         return memorySwap;
     }
 
     @Override
-	public CreateContainerCmdImpl withMemorySwap(long memorySwap) {
+    public CreateContainerCmdImpl withMemorySwap(long memorySwap) {
         this.memorySwap = memorySwap;
         return this;
     }
 
     @Override
-	public int getCpuShares() {
+    public int getCpuShares() {
         return cpuShares;
     }
 
     @Override
-	public CreateContainerCmdImpl withCpuShares(int cpuShares) {
+    public CreateContainerCmdImpl withCpuShares(int cpuShares) {
         this.cpuShares = cpuShares;
         return this;
     }
 
     @Override
-	public boolean isAttachStdin() {
+    public boolean isAttachStdin() {
         return attachStdin;
     }
 
     @Override
-	public CreateContainerCmdImpl withAttachStdin(boolean attachStdin) {
+    public CreateContainerCmdImpl withAttachStdin(boolean attachStdin) {
         this.attachStdin = attachStdin;
         return this;
     }
 
     @Override
-	public boolean isAttachStdout() {
+    public boolean isAttachStdout() {
         return attachStdout;
     }
 
     @Override
-	public CreateContainerCmdImpl withAttachStdout(boolean attachStdout) {
+    public CreateContainerCmdImpl withAttachStdout(boolean attachStdout) {
         this.attachStdout = attachStdout;
         return this;
     }
 
     @Override
-	public boolean isAttachStderr() {
+    public boolean isAttachStderr() {
         return attachStderr;
     }
 
     @Override
-	public CreateContainerCmdImpl withAttachStderr(boolean attachStderr) {
+    public CreateContainerCmdImpl withAttachStderr(boolean attachStderr) {
         this.attachStderr = attachStderr;
         return this;
     }
 
     @Override
-	public String[] getEnv() {
+    public String[] getEnv() {
         return env;
     }
 
     @Override
-	public CreateContainerCmdImpl withEnv(String... env) {
+    public CreateContainerCmdImpl withEnv(String... env) {
         this.env = env;
         return this;
     }
 
     @Override
-	public String[] getCmd() {
+    public String[] getCmd() {
         return cmd;
     }
 
     @Override
-	public CreateContainerCmdImpl withCmd(String... cmd) {
+    public CreateContainerCmdImpl withCmd(String... cmd) {
         this.cmd = cmd;
         return this;
     }
 
     @Override
-	public String[] getDns() {
+    public String[] getDns() {
         return dns;
     }
 
     @Override
-	public CreateContainerCmdImpl withDns(String... dns) {
+    public CreateContainerCmdImpl withDns(String... dns) {
         this.dns = dns;
         return this;
     }
 
     @Override
-	public String getImage() {
+    public String getImage() {
         return image;
     }
 
     @Override
-	public CreateContainerCmdImpl withImage(String image) {
+    public CreateContainerCmdImpl withImage(String image) {
         this.image = image;
         return this;
     }
 
     @Override
-	@JsonIgnore
+    @JsonIgnore
     public Volume[] getVolumes() {
         return volumes.getVolumes();
     }
 
     @Override
-	public CreateContainerCmdImpl withVolumes(Volume... volumes) {
+    public CreateContainerCmdImpl withVolumes(Volume... volumes) {
         this.volumes = new Volumes(volumes);
         return this;
     }
 
     @Override
-	public String[] getVolumesFrom() {
+    public String[] getVolumesFrom() {
         return volumesFrom;
     }
 
     @Override
-	public CreateContainerCmdImpl withVolumesFrom(String... volumesFrom) {
+    public CreateContainerCmdImpl withVolumesFrom(String... volumesFrom) {
         this.volumesFrom = volumesFrom;
         return this;
     }
 
-	@Override
+    @Override
     public String toString() {
         return new ToStringBuilder(this).append("create container ")
-            .append(name != null ? "name=" + name + " " : "")
-            .append(this)
-            .toString();
+                .append(name != null ? "name=" + name + " " : "")
+                .append(this)
+                .toString();
     }
-    
+
     /**
      * @throws NotFoundException No such container
      * @throws ConflictException Named container already exists
      */
-	@Override
+    @Override
     public CreateContainerResponse exec() throws NotFoundException, ConflictException {
-    	return super.exec();
+        return super.exec();
     }
-}    
+}
